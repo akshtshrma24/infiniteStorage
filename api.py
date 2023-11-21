7a80235c74c97725d5bbebd757793e8bb2cd2558
akshtshrma24 <akshitsharmaa65@gmail.com>
2023-11-21 11:05:25 -0800

    return html_content

--- api.py
+++ api.py
@@ -9,10 +9,10 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 UPLOAD_DIR = Path("uploads")
 UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
+html_content = open("./templates/index.html", "r").read()
 
 @app.get("/", response_class=HTMLResponse)
 async def home():
-    html_content = open("./templates/index.html", "r").read()
     return HTMLResponse(content=html_content, status_code=200)
 
 @app.post("/uploadfile/")
@@ -20,5 +20,4 @@ async def create_upload_file(file: UploadFile = File(...)):
     file_path = f"{UPLOAD_DIR}/{file.filename}"
     with open(file_path, "wb") as f:
         f.write(file.file.read())
-
-    return {"filename": file.filename, "file_path": file_path}
+    return HTMLResponse(content=html_content, status_code=200)
